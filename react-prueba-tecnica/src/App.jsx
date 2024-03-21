f46267d7035e297c6ae26a04788574c777cc99c6
PabloJL <jup_jim_7@hotmail.com>
2024-03-21 17:37:30 -0600

    Project config and fetching data

---
+++ react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
+
+function App() {
+  const [fact, setFact] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact));
+  }, []);
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  );
+}
+
+export default App;

710bb2252ba1dd8c25754a7bbcabf25f83cbd4e6
PabloJL <jup_jim_7@hotmail.com>
2024-03-21 18:21:44 -0600

    Fetching img not working

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,46 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
+const CAT_PREFIX_IMG_URL = "https://cataas.com";
 
 function App() {
   const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const threeFirstWords = fact.split(" ", 3).join(" ");
+        //para mas palabras
+        // const firstWord = fact.split(' ').slice[0,3].join(' ')
+
+        fetch(
+          `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(url);
+          });
+      });
   }, []);
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMG_URL}${imageUrl}`}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
     </main>
   );
 }

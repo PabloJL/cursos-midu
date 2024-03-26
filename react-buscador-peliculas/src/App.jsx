50b713fc49a72bba18eae3a96543cac251f07c73
PabloJL <jup_jim_7@hotmail.com>
2024-03-25 19:32:51 -0600

    Form validations using custom hook

--- react-buscador-peliculas/src/App.jsx
+++ react-buscador-peliculas/src/App.jsx
@@ -1,21 +1,77 @@
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/movies";
 import { useMovies } from "./hooks/useMovies";
 
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una película con un número");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
+
 function App() {
-  const { movies: mappedMovies } = useMovies();
+  const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //No conctolada, este es un objeto que sacamos del DOM al momento de hacer submit
+    //const { query } = Object.fromEntries(new window.FormData(event.target));
+
+    //aqui se pueden hacer validaciones
+    //console.log({ query });
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de Peliculas</h1>
-        <form className="form">
-          <input type="text" placeholder="Avengers, Star Wars, IT" />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name="query"
+            value={search}
+            onChange={handleChange}
+            type="text"
+            placeholder="Avengers, Star Wars, IT"
+          />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
--- react-buscador-peliculas/src/components/movies.jsx
+++ react-buscador-peliculas/src/components/movies.jsx
@@ -3,7 +3,7 @@ function ListOfMovies({ movies }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li key={movie.imdbID}>
+        <li key={movie.id}>
           <h3>{movie.title}</h3>
           <p>{movie.year}</p>
           <img src={movie.poster} alt={movie.title} />

5654a388cbc6a99655bf6e859dbab1e1df5075ba
PabloJL <jup_jim_7@hotmail.com>
2024-04-01 17:56:06 -0600

    Components and logic added

--- google-translate-clone/src/App.tsx
+++ google-translate-clone/src/App.tsx
@@ -1,25 +1,65 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ArrowsIcon } from "./components/Icons";
 import { useStore } from "./hooks/useStore";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, Stack } from "react-bootstrap";
+import { LanguageSelector } from "./components/LanguageSelector";
+import { SectionType } from "./types.d";
 
 function App() {
-  const { fromLanguage, setFromLanguage, toLanguage } = useStore();
+  const {
+    fromLanguage,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    toLanguage,
+  } = useStore();
   return (
     <Container fluid>
       <h1>Google Translate</h1>
       <Row>
-        <Col>
-          <h2>From</h2>
-          {fromLanguage}
+        <Col xs="auto">
+          <Stack gap={2}>
+            <h2>From</h2>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            {fromLanguage}
+            <Form.Control
+              as="textarea"
+              placeholder="Introducir texto"
+              autoFocus
+              style={{ height: "150px" }}
+            />
+          </Stack>
         </Col>
         <Col>
-          <button>Intercambiar</button>
+          <Button
+            variant="link"
+            // disabled={fromLanguage === "auto"}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
         </Col>
 
-        <Col>
-          <h2>To</h2>
-          {toLanguage}
+        <Col xs="auto">
+          <Stack gap={2}>
+            <h2>To</h2>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+            {toLanguage}
+            <Form.Control
+              as="textarea"
+              placeholder="Traduccion"
+              style={{ height: "150px" }}
+            />
+          </Stack>
         </Col>
       </Row>
     </Container>
--- google-translate-clone/src/hooks/useStore.ts
+++ google-translate-clone/src/hooks/useStore.ts
@@ -1,43 +1,73 @@
-import { Action, Language, type State } from "../types";
 import { useReducer } from "react";
+// import { AUTO_LANGUAGE } from '../constants'
+import {
+  type FromLanguage,
+  type Language,
+  type Action,
+  type State,
+} from "../types";
 
+// 1. Create a initialState
 const initialState: State = {
-  fromLanguage: "auto",
+  fromLanguage: "es",
   toLanguage: "en",
   fromText: "",
   result: "",
   loading: false,
 };
 
+// 2. Create a reducer
 function reducer(state: State, action: Action) {
   const { type } = action;
+
   if (type === "INTERCHANGE_LANGUAGES") {
+    // lógica del estado dentro del reducer
+    // porque lo evitamos en los componentes
+    // if (state.fromLanguage === AUTO_LANGUAGE) return state
+
+    const loading = state.fromText !== "";
+
     return {
       ...state,
+      loading,
+      result: "",
       fromLanguage: state.toLanguage,
       toLanguage: state.fromLanguage,
     };
   }
 
   if (type === "SET_FROM_LANGUAGE") {
+    if (state.fromLanguage === action.payload) return state;
+
+    const loading = state.fromText !== "";
+
     return {
       ...state,
       fromLanguage: action.payload,
+      result: "",
+      loading,
     };
   }
 
   if (type === "SET_TO_LANGUAGE") {
+    if (state.toLanguage === action.payload) return state;
+    const loading = state.fromText !== "";
+
     return {
       ...state,
-      fromText: action.payload,
+      toLanguage: action.payload,
+      result: "",
+      loading,
     };
   }
 
   if (type === "SET_FROM_TEXT") {
+    const loading = action.payload !== "";
+
     return {
       ...state,
-      loading: true,
-      fromLanguage: action.payload,
+      loading,
+      fromText: action.payload,
       result: "",
     };
   }
@@ -54,20 +84,30 @@ function reducer(state: State, action: Action) {
 }
 
 export function useStore() {
-  //no retornar el dispatch directamente. Dentro del hook exportamos un contrato
+  // 3. usar el hook useReducer
   const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
     useReducer(reducer, initialState);
 
-  const interchangeLanguages = () =>
+  const interchangeLanguages = () => {
     dispatch({ type: "INTERCHANGE_LANGUAGES" });
-  const setFromLanguage = (payload: Language) =>
+  };
+
+  const setFromLanguage = (payload: FromLanguage) => {
     dispatch({ type: "SET_FROM_LANGUAGE", payload });
-  const setToLanguage = (payload: Language) =>
+  };
+
+  const setToLanguage = (payload: Language) => {
     dispatch({ type: "SET_TO_LANGUAGE", payload });
-  const setFromText = (payload: string) =>
+  };
+
+  const setFromText = (payload: string) => {
     dispatch({ type: "SET_FROM_TEXT", payload });
-  const setResult = (payload: string) =>
+  };
+
+  const setResult = (payload: string) => {
     dispatch({ type: "SET_RESULT", payload });
+  };
+
   return {
     fromLanguage,
     toLanguage,

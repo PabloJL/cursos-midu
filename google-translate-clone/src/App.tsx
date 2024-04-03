0c245ce8631806ebd6e17b4fa4ebc19edce6fd99
PabloJL <jup_jim_7@hotmail.com>
2024-04-02 18:01:52 -0600

    Few changes

--- google-translate-clone/src/App.tsx
+++ google-translate-clone/src/App.tsx
@@ -1,19 +1,53 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ArrowsIcon } from "./components/Icons";
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from "./components/Icons";
 import { useStore } from "./hooks/useStore";
-import { Container, Row, Col, Button, Form, Stack } from "react-bootstrap";
+import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 import { LanguageSelector } from "./components/LanguageSelector";
 import { SectionType } from "./types.d";
+import { TextArea } from "./components/TextArea";
+import { useEffect } from "react";
+import { translate } from "./services/translate";
+import { useDebounce } from "./hooks/useDebounce";
+import { VOICE_FOR_LANGUAGE } from "./constants";
 
 function App() {
   const {
     fromLanguage,
+    fromText,
+    result,
     interchangeLanguages,
     setFromLanguage,
     setToLanguage,
+    setFromText,
+    loading,
+    setResult,
     toLanguage,
   } = useStore();
+
+  const debounceFromText = useDebounce(fromText);
+
+  const handleClipBoard = () => {
+    navigator.clipboard.writeText(result).catch(() => {});
+  };
+
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(result);
+    utterance.lang = VOICE_FOR_LANGUAGE[toLanguage];
+    utterance.rate = 0.9;
+    speechSynthesis.speak(utterance);
+  };
+
+  useEffect(() => {
+    if (debounceFromText === "") return;
+    translate({ fromLanguage, toLanguage, text: debounceFromText })
+      .then((result) => {
+        if (result == null) return;
+        setResult(result);
+      })
+      .catch(() => setResult("error"));
+  }, [debounceFromText, fromLanguage, toLanguage]);
+
   return (
     <Container fluid>
       <h1>Google Translate</h1>
@@ -27,11 +61,10 @@ function App() {
               onChange={setFromLanguage}
             />
             {fromLanguage}
-            <Form.Control
-              as="textarea"
-              placeholder="Introducir texto"
-              autoFocus
-              style={{ height: "150px" }}
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
             />
           </Stack>
         </Col>
@@ -53,12 +86,29 @@ function App() {
               value={toLanguage}
               onChange={setToLanguage}
             />
-            {toLanguage}
-            <Form.Control
-              as="textarea"
-              placeholder="Traduccion"
-              style={{ height: "150px" }}
-            />
+            <div style={{ position: "relative" }}>
+              <TextArea
+                loading={loading}
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  bottom: 0,
+                  display: "flex",
+                }}
+              >
+                <Button variant="link" onClick={handleClipBoard}>
+                  <ClipboardIcon />
+                </Button>
+                <Button variant="link" onClick={handleSpeak}>
+                  <SpeakerIcon />
+                </Button>
+              </div>
+            </div>
           </Stack>
         </Col>
       </Row>
